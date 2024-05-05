665c8b8d5962f7ea901ac3ba76d8bd1895765c17
DESKTOP-IJR8QVP\Mohammed <mohammedadel9yc@gmail.com>
2024-05-05 03:32:30 +0300

    _firebase android setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
 import 'store_app.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: "AIzaSyDBXAt18E-tcFaQKZieJRRNAM4aM6D2E9M",
+              appId: "1:562037944014:android:cc6c5c2fbebab9d4e56a9f",
+              messagingSenderId: "562037944014",
+              projectId: "store-5df85"))
+      : await Firebase.initializeApp();
+
   runApp(const StoreApp());
 }
-
-
