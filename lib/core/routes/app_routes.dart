98e019dfa31a90993a692a6f9937fa5ccb3011ce
DESKTOP-IJR8QVP\Mohammed <mohammedadel9yc@gmail.com>
2024-05-25 23:45:51 +0300

    _added onboarding screen

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,22 +1,23 @@
 import 'package:store/home.dart';
 import 'package:flutter/material.dart';
 import 'package:store/test1_screen.dart';
-import 'package:store/test2_screen.dart';
 import 'package:store/core/routes/base_routes.dart';
+import 'package:store/features/onboarding/onboarding_screen.dart';
+
 
 
 class AppRoutes {
   static const String testOne = '/test1';
   static const String testTwo = '/test2';
   static const String home = '/homescreen';
-
+  static const String onboarding = '/onboardingScreen';
   static Route<void> onGenerateRoute(RouteSettings settings) {
     final arg = settings.arguments;
     switch (settings.name) {
       case AppRoutes.testOne:
         return BaseRoute(page: test1());
-      case AppRoutes.testTwo:
-        return BaseRoute(page: test2());
+      case AppRoutes.onboarding:
+        return BaseRoute(page: const OnboardingScreen());
       default:
         return BaseRoute(page: homescreen());
     }
