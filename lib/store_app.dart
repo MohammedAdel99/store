196c6b47e585ca0a55ca62ee784e22015fdb7373
DESKTOP-IJR8QVP\Mohammed <mohammedadel9yc@gmail.com>
2024-05-25 04:20:55 +0300

    _some updates

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -5,6 +5,7 @@ class StoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return MaterialApp(
       debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
       title: 'Flutter Demo',
@@ -12,6 +13,34 @@ class StoreApp extends StatelessWidget {
       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+=======
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+        title: 'Flutter Demo',
+<<<<<<< Updated upstream
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Center(child: Text('My Store')),
+          ),
+        ),
+=======
+        theme:ThemeDark(),
+        locale:const Locale('en') ,
+        supportedLocales:AppLocalizationsSetup.supportedLocales ,
+        localeResolutionCallback: AppLocalizationsSetup.localeResolutionCallback,
+        localizationsDelegates:AppLocalizationsSetup.localizationsDelegates ,
+
+        onGenerateRoute: AppRoutes.onGenerateRoute,
+        initialRoute: AppRoutes.home,
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
       ),
       home: Scaffold(
         appBar:AppBar(
