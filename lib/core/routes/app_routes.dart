f08c65b4d9a1be57715b959d697748e105e3f5b4
DESKTOP-IJR8QVP\Mohammed <mohammedadel9yc@gmail.com>
2024-06-04 04:43:42 +0300

    _added login screen ui.

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:store/core/routes/base_routes.dart';
 import 'package:store/features/onboarding/onboarding_screen.dart';
 import 'package:store/features/auth/login/presentation/screens/login_screen.dart';
+import 'package:store/features/auth/signUp/presentation/screens/sign_up_screen.dart';
 
 
 
@@ -12,7 +13,8 @@ import 'package:store/features/auth/login/presentation/screens/login_screen.dart
 class AppRoutes {
   static const String home = '/homescreen';
 
-  static const String login = '/lodin';
+  static const String login = '/login';
+
 
   static const String onboarding = '/OnboardingScreen';
   static Route<void> onGenerateRoute(RouteSettings settings) {
@@ -26,6 +28,8 @@ class AppRoutes {
         
       case AppRoutes.login:
         return BaseRoute(page: const LoginScreen());
+     
+  
 
       default:
         return BaseRoute(page: homescreen());
